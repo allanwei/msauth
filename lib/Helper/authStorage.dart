71ff644e32db9c190595938aaa9a7ce0d907242c
Allan Wei <AWei@smithandlong.com>
2020-10-15 11:31:27 -0400

    try add web support

--- lib/Helper/authStorage.dart
+++ lib/Helper/authStorage.dart
@@ -1,4 +1,7 @@
 import 'dart:async';
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:flutter/foundation.dart';
+
 import '../Model/user.dart';
 import '../Model/token.dart';
 import "dart:convert" as Convert;
@@ -6,21 +9,26 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthStorage {
   static AuthStorage shared = new AuthStorage();
-  FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+  dynamic _secureStorage = kIsWeb
+      ? new PersistCookieJar(
+          dir: "./cookies",
+          ignoreExpires: true, //save/load even cookies that have expired.
+        )
+      : new FlutterSecureStorage();
   final String _identifier = "Token";
-  final String _user ="User";
+  final String _user = "User";
 
-  Future<void> saveTokenToCache(Token token,User user) async {
+  Future<void> saveTokenToCache(Token token, User user) async {
     var data = Token.toJsonMap(token);
     var json = Convert.jsonEncode(data);
-    await _secureStorage.write(key:_identifier, value: json);
-    var userData=User.toJsonMap(user);
-    var userJson=Convert.jsonEncode(userData);
+    await _secureStorage.write(key: _identifier, value: json);
+    var userData = User.toJsonMap(user);
+    var userJson = Convert.jsonEncode(userData);
     await _secureStorage.write(key: _user, value: userJson);
   }
 
   Future<T> loadTokenToCache<T extends Token>() async {
-    var json = await _secureStorage.read(key:_identifier);
+    var json = await _secureStorage.read(key: _identifier);
     if (json == null) return null;
     try {
       var data = Convert.jsonDecode(json);
@@ -30,13 +38,14 @@ class AuthStorage {
       return null;
     }
   }
-  Future<T> loadUserToCache<T extends User>() async{
-    var json=await _secureStorage.read(key: _user);
-    if(json==null) return null;
-    try{
-      var data=Convert.jsonDecode(json);
+
+  Future<T> loadUserToCache<T extends User>() async {
+    var json = await _secureStorage.read(key: _user);
+    if (json == null) return null;
+    try {
+      var data = Convert.jsonDecode(json);
       return _getUserFromMap<T>(data);
-    } catch(exception){
+    } catch (exception) {
       print(exception);
       return null;
     }
@@ -44,10 +53,10 @@ class AuthStorage {
 
   Token _getTokenFromMap<T extends Token>(Map<String, dynamic> data) =>
       Token.fromJson(data);
-  User _getUserFromMap<T extends User>(Map<String,dynamic>data)=>
+  User _getUserFromMap<T extends User>(Map<String, dynamic> data) =>
       User.fromJson(data);
 
   Future clear() async {
-    _secureStorage.delete(key:_identifier);
+    _secureStorage.delete(key: _identifier);
   }
 }
